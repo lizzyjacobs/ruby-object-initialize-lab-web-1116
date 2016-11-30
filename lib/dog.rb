3a2f4afac99432c9df7b84a9d09ea36e8b0bf4cc
Elizabeth Jacobs <elizabethjjacobs@gmail.com>
2016-11-30 14:20:56 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name(name)
+    @name = name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed(breed)
+    @breed = breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+
+end
